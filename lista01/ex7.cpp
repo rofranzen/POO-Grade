cb176ea33c2a458a995e376b1815fc02ee0c8aed
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-09-27 19:32:37 -0300

    Update ex7.cpp

--- lista01/ex7.cpp
+++ lista01/ex7.cpp
@@ -6,10 +6,23 @@ using namespace std;
 
 int seq(int n, int k, int *ini){
     int soma = 0;
+    int resp[n+1];
+    int fim;
+    
     if (n >= k){
-        for(int i = n-k; i <n; i++){
-            soma += (ini[i]*ini[i]);
+        for(int i = 0; i<k;i++)
+            resp[i] = ini[i]
+
+        
+        for(int i = k; i <= n; i++){
+            soma = 0;
+            for(int j =0; j<i; j++){
+                soma = soma + (ini[i]*ini[i]);
+            }
+            resp[i] = soma;
         }
+        fim = resp[n];
+        return fim;
     }
     else{
         soma = ini[n];
@@ -22,9 +35,11 @@ int seq(int n, int k, int *ini){
 int main()
 {
     int a, b;
-    int t[] = {1, -2, 1};
 
     cin >>a >> b;
+    for(int i = 0; i<k;i++)
+        cin>>t[i];
+    
     cout << seq(a,b,t) << endl;
     return 0;
 }
