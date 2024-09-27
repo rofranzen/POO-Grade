0ae2dca825f2f89c8336327c6c415b717a62c13f
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-09-27 16:27:31 -0300

    Update ex11.cpp

--- lista01/ex11.cpp
+++ lista01/ex11.cpp
@@ -9,27 +9,34 @@ using namespace std;
 
 std::string raizes(double a, double b, double c){
     double delta = pow(b,2) - 4*a*c;
+    cout << delta << endl;
 
 
-    if (delta < 0){
+    if (delta < 0 || (a==0 && b==0)){
         return "Nenhuma";
     }
-    else if (delta == 0 && c == 0){
+    else if (c == 0){
         return "Infinitas";
+    }
+    else if (a == 0){
+        return to_string( ((-c)/b) );
     }
 
     delta = sqrt(delta);
+    cout <<delta<<endl;
 
-    string resposta;
 
-    double x1 = (-b + delta)/(2*a);
-    double x2 = (-b- delta) / (2*a);
+    double x1 = ((-b) + delta)/(2*a);
+    double x2 = ((-b)- delta) / (2*a);
     //cout << x1 << " e " << x2 << endl;
 
 
     if (x1 == x2){
         return to_string(x1);
     }
+
+    string resposta;
+    
     resposta.append(to_string(x1));
     resposta.append(" e ");
     resposta.append(to_string(x2));
