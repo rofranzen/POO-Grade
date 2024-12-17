f8d42d62a3fbca97c473a8a2ee7973ce9d53b97d
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-12-17 00:30:14 -0300

    Add files via upload

---
+++ lista02/ex3.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <math.h>
+
+class Vetor
+{
+    int _x;
+    int _y;
+
+public:
+    Vetor(int x=0, int y=0){
+        _x = x;
+        _y = y;
+    };
+    friend Vetor operator+(const Vetor& a, const Vetor& b){
+        return Vetor(a._x + b._x, a._y + b._y);
+    };
+    friend Vetor operator-(const Vetor& a, const Vetor& b){
+        return Vetor(a._x - b._x, a._y - b._y);
+    };
+    friend int operator*(const Vetor& a, const Vetor& b){
+        return (a._x * b._x + a._y * b._y);
+    };
+    friend int operator^(const Vetor& a, const Vetor& b){
+        return ( a._x * b._y - a._y * b._x);
+    };
+    double abs(){
+        return sqrt(_x*_x + _y*_y);
+    };
+
+    friend double operator<(const Vetor& a, const Vetor& b){
+        Vetor c = a;
+        Vetor d = b;
+        return acos((a*b)/(c.abs()* d.abs()))*180/3.1415;
+    };
+
+    friend std::ostream& operator<<(std::ostream &out, const Vetor &a){
+        out << "(" << a._x << ", " << a._y << ")";
+        return out;
+    };
+
+};
---
+++ lista02/ex4.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <math.h>
+
+class Polinomio2
+{
+    int _a;
+    int _b;
+    int _c;
+
+
+public:
+    Polinomio2(int a =0, int b =0, int c=0){
+        if (a == 0){
+            _a = 1;
+            _b = 0;
+            _c = 0;
+        }
+        _a = a;
+        _b = b;
+        _c = c;
+        //std::cout << "criado";
+
+    };
+    double *raizes(){
+        //int qtd;
+        double t1,t2, *p = new double[3]();
+
+        double t = _b*_b - 4*_a*_c;
+        if (t<0){
+            //qtd = 0;
+            //std::cout << "chegou no menor que 0";
+            p[0] = 0;;
+            return p;
+        };
+
+        t = sqrt(t);
+
+        t1 = t - _b;
+        t2 = -t - _b;
+        if(t1 == t2){
+            //qtd = 1;
+            t1 = t1/(2*_a);
+            //std::cout << "chegou no 0";
+            p[0] = 1;
+            p[1] = t1;
+            return p;
+        };
+        t1 = t1/(2*_a);
+        t2 = t2/(2*_a);
+
+        p[0] = 2;
+        p[1] = t2;
+        p[2] = t1;
+        return p;
+    };
+
+    double operator()(double x){
+        double seg = _a * x;
+        return (seg * seg + _b * x + _c);
+    };
+
+};
