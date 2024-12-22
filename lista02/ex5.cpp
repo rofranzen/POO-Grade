9c921eb8c1bf903aa438acfd9a08be09072b00c4
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-12-22 13:05:20 -0300

    Update ex5.cpp

--- lista02/ex5.cpp
+++ lista02/ex5.cpp
@@ -1,49 +1,43 @@
 #include <iostream>
-#include <stdio.h>
 #include <fstream>
+#include <string>
 
 class Arquivo
 {
     std::string _conteudo;
-    int _p;
-    int _tam;
 
 public:
-    Arquivo(std::string arquivo): _p(0){
+
+    Arquivo(std::string arquivo) {
         std::ifstream arq(arquivo);
-        _tam = 1;
-
-        std::string linha;
-        while (std::getline(arq, linha)) {
-            _conteudo += linha + "\n";  // Adiciona cada linha lida ao conteúdo
-            _tam++;
-        }
-
-        arq.close();  // Fecha o arquivo
-
-    };
-
-    std::string proxima_linha(){
-
-        if (_p > _tam) {
-            _p = 0;
-        };
-
-        size_t inicio = _p;
-        size_t fim = _conteudo.find('\n', inicio);  // Encontra a posição da próxima quebra de linha
-
-        if (fim == std::string::npos) {  // Se não encontrar '\n', significa que é a última linha
-            _p = _conteudo.size();  // Marca o fim
-            return _conteudo.substr(inicio);  // Retorna a última linha
+        if (arq.is_open()) {
+            std::string linha;
+            while (std::getline(arq, linha)) {
+                _conteudo += linha + "\n";
+            }
+            arq.close();
         } else {
-            _p = fim + 1;  // Avança para a próxima linha
-            return _conteudo.substr(inicio, fim - inicio);  // Retorna a linha
+            std::cerr << "Erro ao abrir o arquivo!" << std::endl;
+        }
+    }
+    std::string proxima_linha() {
+        static size_t pos = 0;
+
+        if (pos >= _conteudo.size()) {
+            return "";
         }
 
-        _p++;
-    };
+        size_t inicio = pos;
+        size_t fim = _conteudo.find('\n', inicio);
 
-    ~Arquivo(){
-
-    };
+        if (fim == std::string::npos) {
+            pos = _conteudo.size();
+            return _conteudo.substr(inicio);
+        } else {
+            pos = fim + 1;
+            return _conteudo.substr(inicio, fim - inicio);
+        }
+    }
+    ~Arquivo() {
+    }
 };
