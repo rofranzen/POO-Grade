88ebf1fad6d0b0e3fdb193467d26d4560453e897
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-12-22 19:36:15 -0300

    Update ex3.cpp

--- lista02e/ex3.cpp
+++ lista02e/ex3.cpp
@@ -1,12 +1,12 @@
-#include <iostream>
-#include <fstream>
 #include <string>
 
-class Arquivo {
+class Arquivo
+{
     std::string _conteudo;
 
 public:
-    Arquivo(const std::string& arquivo) {
+
+    Arquivo(std::string arquivo) {
         std::ifstream arq(arquivo);
         if (arq.is_open()) {
             std::string linha;
@@ -14,53 +14,28 @@ public:
                 _conteudo += linha + "\n";
             }
             arq.close();
+        } else {
+            std::cerr << "Erro ao abrir o arquivo!" << std::endl;
         }
     }
+    std::string proxima_linha() {
+        static size_t pos = 0;
 
-    std::string proxima_linha(size_t& pos) const{
-        if (pos >= _conteudo.size()){
+        if (pos >= _conteudo.size()) {
             return "";
         }
+
         size_t inicio = pos;
         size_t fim = _conteudo.find('\n', inicio);
 
-        if(fim == std::string::npos) {
+        if (fim == std::string::npos) {
             pos = _conteudo.size();
             return _conteudo.substr(inicio);
-        }
-        else{
+        } else {
             pos = fim + 1;
             return _conteudo.substr(inicio, fim - inicio);
         }
     }
-
-class Iterator {
-    const Arquivo& _arquivo;
-    size_t _pos;
-
-public:
-    Iterator(const Arquivo& arquivo, size_t pos) : _arquivo(arquivo), _pos(pos) {}
-
-    bool operator!=(const Iterator& other) const{
-        return _pos != other._pos;
+    ~Arquivo() {
     }
-
-    std::string operator*() {
-        return _arquivo.proxima_linha(_pos);
-
-    }
-
-    Iterator& operator++(){
-
-        return *this;
-    }
-};
-
-Iterator end() const{
-    return Iterator(*this, _conteudo.size());
-}
-
-Iterator begin() const{
-    return Iterator(*this, 0);
-}
 };
