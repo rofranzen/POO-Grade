c534d6da4ae89163de96a5c52de39745b95f49ce
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-12-22 23:40:22 -0300

    Update ex11.cpp

--- lista04/ex11.cpp
+++ lista04/ex11.cpp
@@ -12,39 +12,26 @@ private:
     std::string pais;
 
 public:
-    Pessoa(const std::string& dados) {
+        Pessoa(const std::string& dados){
+             size_t pos_tem = dados.find(" tem "); //P poder noem composto
+             nome = dados.substr(0, pos_tem);
+             
+             
+             size_t pos_idade = pos_tem + 5; //pula lixo
+             idade = std::stoi(dados.substr(pos_idade, 2)); 
+             size_t pos_mora = dados.find(" e mora em ") + 11; //11 tb pula lixo
+             size_t pos_uf = dados.find(" - ", pos_mora); 
+             
+             cidade = dados.substr(pos_mora, pos_uf - pos_mora); 
+             size_t pos_pais = dados.find(", ", pos_uf) + 2; 
+             uf = dados.substr(pos_uf + 3, 2); pais = dados.substr(pos_pais); 
+         } 
+             
+        friend std::ostream& operator<<(std::ostream& os, const Pessoa& p){
+            os << p.nome << "\n" << p.idade
+             << "\n" << p.cidade << "\n" << p.uf << "\n" << p.pais; 
+            return os; }
 
-        std::istringstream stream(dados); //dados fornecidos
-        std::string temp;
 
-        //pega tb nome compost
-        std::getline(stream, nome, ' '); 
-        while (stream >> temp && temp != "tem") {
-            nome += " " + temp;  //faz append do resto dps d aprimeira
-        }
 
-        //get age
-        stream >> idade;
-        //std::cout << "temp is "<<idade;
-
-        //pula anos
-        stream >> temp;
-        //;;std::cout << "temp is "<< temp;
-
-        //pula lixo
-        stream >> temp >> temp >> temp;
-
-        std::getline(stream, temp, ' '); //pula espaco
-        std::getline(stream, cidade, '-');
-        //stream >> temp;
-        std::getline(stream, uf, ',');
-        std::getline(stream, temp, ' '); //pula espaco
-        std::getline(stream, pais);         // The rest is the country
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Pessoa& p) {
-        os << p.nome << "\n" << p.idade << "\n"
-           << p.cidade << "\n" << p.uf << "\n" << p.pais;
-        return os;
-    }
 };
