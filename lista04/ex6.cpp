cfe171e3d3468cb2bb81dc59c1d509097fd93041
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-12-22 22:55:43 -0300

    Update ex6.cpp

--- lista04/ex6.cpp
+++ lista04/ex6.cpp
@@ -9,32 +9,31 @@ using namespace std;
 
 std::vector<int> retira(std::vector<int> v, int n)
 {
-    
-    //bubble sort 
+
+    //bubble sort
     int size=v.size();
     for(int i=0;i<size - 1;++i){ //i++
-            
+
        for (int j = 0; j < size - 1 - i; ++j){ //j++
-           
-          if (v[j] > v[j + 1])
-          {
+
+
                     //troca el
                 int temp = v[j] * (v[j] > v[j + 1]) + v[j + 1] * (v[j] <= v[j + 1]);
                 v[j + 1] = v[j] * (v[j] <= v[j + 1]) + v[j + 1] * (v[j] > v[j + 1]);
-                v[j] = temp; 
+                v[j] = temp;
                 //cout << v[j+1] << endl;
-          }
+
         }
     }
 
     std::vector<int> result;
-    
+
    for (int i = 0; i < size; ++i) //i++
     {
         bool is_in_result=false; // sem if dessa vez
-   
+
         for (int j = 0; j < result.size(); ++j){ //j++
-                
+
             is_in_result = is_in_result || (result[j] == v[i]);
         }
 
