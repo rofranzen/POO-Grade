717e5df1a300dc398ef8c708af3f84b552a7d583
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-12-22 22:33:57 -0300

    Update ex6.cpp

--- lista04/ex6.cpp
+++ lista04/ex6.cpp
@@ -9,43 +9,38 @@ using namespace std;
 
 std::vector<int> retira(std::vector<int> v, int n)
 {
-
-    //bubble sort
+    
+    //bubble sort 
     int size=v.size();
     for(int i=0;i<size - 1;++i){ //i++
-
+            
        for (int j = 0; j < size - 1 - i; ++j){ //j++
-
+           
           if (v[j] > v[j + 1])
           {
                     //troca el
-                int temp = v[j]; //temp
-                v[j] = v[j + 1];
-                v[j + 1] = temp;
+                int temp = v[j] * (v[j] > v[j + 1]) + v[j + 1] * (v[j] <= v[j + 1]);
+                v[j + 1] = v[j] * (v[j] <= v[j + 1]) + v[j + 1] * (v[j] > v[j + 1]);
+                v[j] = temp; 
                 //cout << v[j+1] << endl;
           }
         }
     }
 
     std::vector<int> result;
-
-for (int i = 0; i < size; ++i) {
-        bool found = false; //se nao igual n
-
-        for (int j = 0; j < result.size(); ++j){
-            if (result[j] == v[i]) {
-
-                found = true;
-                break;
-
-              }
+    
+   for (int i = 0; i < size; ++i) //i++
+    {
+        bool is_in_result=false; // sem if dessa vez
+   
+        for (int j = 0; j < result.size(); ++j){ //j++
+                
+            is_in_result = is_in_result || (result[j] == v[i]);
         }
 
-        //entao se n for adiciona
-        if (!found && v[i] != n)
-            {
-            result.push_back( v[i] );
-        }
+        result.insert(result.end(),
+                      (is_in_result || v[i] == n) ? 0 :
+                      1, v[i]);
     }
 
     return result;
