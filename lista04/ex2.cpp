b2fafc25290ae42daf196d9dfbc28cdc9b4894bf
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-12-22 21:39:31 -0300

    Update ex2.cpp

--- lista04/ex2.cpp
+++ lista04/ex2.cpp
@@ -3,38 +3,58 @@
 #include <queue>
 #include <string>
 
+
 std::string jogo(std::stack<int> cartas, std::queue<std::string> jogadores){
+    //vec soma
 
-    int soma[3] = {0,0,0};
-    std::string nomes[3];
-    for (int i = 0; i < 3; i++) {
-        nomes[i] = jogadores.front();
-        jogadores.pop();
-        //cout << "chegouu";
-    };
 
-    int i =0;
-    while(!cartas.empty() ){
-        int carta = cartas.top();
-        cartas.pop();
+    std::vector<int> soma;
 
-        soma[i] += carta;
-        i = (i + 1) % 3;
+    // noms
+    std::vector<std::string> nomes;
+
+    while ( !jogadores.empty()){ //Ate ficar vazio
+        nomes.push_back( jogadores.front()   );
+        soma.push_back(0);
+
+
+        jogadores.pop(); //Tira um por um addicionado
     }
 
+    int i = 0;
+
+    while( !cartas.empty())
+
+
+        {
+        int carta = cartas.top();  //Pega topo
+        cartas.pop(); //Remove uma
+
+        soma[i]+= carta;
+        i=(i +1)% nomes.size() ;
+    }
+
+    // Debug
+    //std::cout << "Somas das cartas:" << std::endl; //weird stuff happening
+    /*for (int i = 0; i < nomes.size(); i++) {
+
+        std::cout << nomes[i]<< ": "<< soma[i] << std::endl;
+    };*/
+
+    // Ve venc e empate
     int vencedor_index = 0;
+
     int menor_soma = soma[0];
-    //cout << "soma[0]" << soma[0];
 
-
-
-    for (int j = 1; j < 3; ++j){
-        if (soma[j] < menor_soma || (soma[j]==menor_soma && nomes[j]<nomes[vencedor_index])) {
-            vencedor_index = j;
+    for (int j = 1; j < nomes.size(); ++j)
+    {
+    if (soma[j] < menor_soma || (soma[j] == menor_soma && nomes[j]< nomes[vencedor_index]))
+    {
+        vencedor_index = j;
             menor_soma = soma[j];
-            
         }
     }
 
-    return nomes[vencedor_index]; //return deu
-}
+    return nomes[vencedor_index]; //agr ta certo
+};
+
